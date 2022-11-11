cbc8dbb4a91447cc3bd80fba384715dfa5cb0d79
Ganesh Raghunadh Mukkapati <112146698+GaneshMukkapati@users.noreply.github.com>
2022-11-11 12:20:39 -0600

    Update

--- controllers/house.js
+++ controllers/house.js
@@ -1,59 +1,90 @@
-var House = require('../models/house'); 
- 
-// List of all Costumes 
-exports.house_list = async function(req, res) { 
-    try{ 
-        theHouses = await House.find(); 
-        res.send(theHouses); 
-    } 
-    catch(err){ 
-        res.status(500); 
-        res.send(`{"error": ${err}}`); 
-    }   
-};  
- 
-// for a specific Costume. 
-exports.house_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: House detail: ' + req.params.id); 
-}; 
-exports.house_view_all_Page = async function(req, res) { 
-    try{ 
-        theHouses = await House.find(); 
-        res.render('House', { title: 'Costume Search Results', results: theHouses }); 
-    } 
-    catch(err){ 
-        res.status(500); 
-        res.send(`{"error": ${err}}`); 
-    }   
-}; 
- 
-// Handle Costume create on POST. 
-exports.house_create_post = async function(req, res) { 
-    console.log(req.body) 
-    let document = new House(); 
-    // We are looking for a body, since POST does not have query parameters. 
-    // Even though bodies can be in many different formats, we will be picky 
-    // and require that it be a json object 
-    // {"costume_type":"goat", "cost":12, "size":"large"} 
-    document.House_Name = req.body.House_Name; 
-    document.Housing_Company = req.body.Housing_Company; 
-    document.Housing_Company_Rating = req.body.Housing_Company_Rating; 
-    try{ 
-        let result = await document.save(); 
-        res.send(result); 
-    } 
-    catch(err){ 
-        res.status(500); 
-        res.send(`{"error": ${err}}`); 
-    }   
-};  
- 
-// Handle Costume delete form on DELETE. 
-exports.house_delete = function(req, res) { 
-    res.send('NOT IMPLEMENTED: House delete DELETE ' + req.params.id); 
-}; 
- 
-// Handle House update form on PUT. 
-exports.house_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: House update PUT' + req.params.id); 
-}; 
+var house = require('../models/house');
+// List of all Costumes
+exports.house_list = function(req, res) {
+ res.send('NOT IMPLEMENTED: house list');
+};
+// for a specific Costume.
+exports.house_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await house.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+
+// Handle Costume create on POST.
+exports.house_create_post = function(req, res) {
+ res.send('NOT IMPLEMENTED: house create POST');
+};
+// Handle Costume delete form on DELETE.
+exports.house_delete = function(req, res) {
+ res.send('NOT IMPLEMENTED: house delete DELETE ' + req.params.id);
+};
+// Handle Costume update form on PUT.
+exports.house_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await house.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.House_Name)
+    toUpdate.House_Name = req.body.House_Name;
+    if(req.body.Housing_Company) toUpdate.Housing_Company = req.body.Housing_Company;
+    if(req.body.Housing_Company_Rating) toUpdate.Housing_Company_Rating = req.body.Housing_Company_Rating;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+    }
+    };
+// VIEWS
+
+   // List of all Costumes
+exports.house_list = async function(req, res) {
+    try{
+    thehouse = await house.find();
+    res.send(thehouse);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
+   // VIEWS
+// Handle a show all view
+exports.house_view_all_Page = async function(req, res) {
+    try{
+    thehouse = await house.find();
+    res.render('house', { title: 'house Search Results', results: thehouse });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
+   // Handle Costume create on POST.
+exports.house_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new house();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.monkeyAge = req.body.monkeyAge;
+    document.monkeyName = req.body.monkeyName;
+    document.monkeyBreed = req.body.monkeyBreed;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+}
--- routes/House.js
+++ routes/House.js
@@ -12,5 +12,5 @@ var router = express.Router();
 /* GET costumes */ 
 router.get('/', house_controlers.house_view_all_Page ); 
 module.exports = router; 
-
 module.exports = router;
+
